b84530753489439fedba2b14dedbf3c6e9a13bdc
hohuuthuan <121500317+hohuuthuan@users.noreply.github.com>
2023-01-08 21:56:50 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#eye').click(function(){
+        $(this).toggleClass('open');
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if($(this).hasClass('open')){
+            $(this).prev().attr('type', 'text')
+        }else{
+            $(this).prev().attr('type', 'password')
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#bam').click(function(){
+        alert('Hello bà dà nghèo khổ giữa trời đông cô đơn :))');
+    })
+});
